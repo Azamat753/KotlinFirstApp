4c87260d01ce386f3947a3998ca6e8f363495a24
azamat753 <azamat.nazar99@gmail.com>
2020-06-27 02:02:14 +0600

    update

--- app/src/main/java/com/lawlett/kotlinfirstapp/MainActivity.kt
+++ app/src/main/java/com/lawlett/kotlinfirstapp/MainActivity.kt
@@ -4,35 +4,37 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main2.*
 
 
+@Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 class MainActivity : AppCompatActivity() {
-    lateinit var someText: String
-    lateinit var someText2: String
+   private lateinit var someText: String
+   private lateinit var someText2: String
     var TEXT_EXTRA: String = "someText"
+    var TEXT_EXTRA2: String = "textFromSecond"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-first_edit.text
+        getAnswer()
     }
 
     fun sendForTwo(view: View) {
         someText = first_edit.text.toString()
         val intent = Intent(this, MainActivity2::class.java)
         intent.putExtra(TEXT_EXTRA, someText)
-        startActivity(intent)
+        startActivityForResult(intent,42)
     }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 42 && resultCode == Activity.RESULT_OK) {
-            if (data != null) {
-                someText2=data.getStringExtra("")
-            }
+    fun getAnswer() {
+        if (intent.getStringExtra(TEXT_EXTRA2) != null) {
+            someText2 = intent.getStringExtra(TEXT_EXTRA2)
+            first_edit.setText(someText2)
         }
     }
+
 }
 
 
--- app/src/main/java/com/lawlett/kotlinfirstapp/MainActivity2.kt
+++ app/src/main/java/com/lawlett/kotlinfirstapp/MainActivity2.kt
@@ -1,13 +1,19 @@
 package com.lawlett.kotlinfirstapp
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main2.*
 
 class MainActivity2 : AppCompatActivity() {
-    var someText: String = ""
-    var TEXT_EXTRA: String = "someText"
+    private  var someText: String = ""
+    private  var someText2: String = ""
+    private var EXTRA_TEXT: String = "someText"
+    private var EXTRA_TEXT2: String = "textFromSecond"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,9 +22,14 @@ class MainActivity2 : AppCompatActivity() {
     }
 
     fun getAnswer() {
-        val intent = Intent()
-        someText = intent.getStringExtra(TEXT_EXTRA)
+        someText = intent.getStringExtra(EXTRA_TEXT)
         second_edit.setText(someText)
-        second_edit.text
+    }
+
+    fun sendForFirst(view: View) {
+        someText2 = second_edit.text.toString()
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra(EXTRA_TEXT2, someText2)
+        startActivityForResult(intent,43)
     }
 }
